5349f90faab9cde616908fd4257cbf6d72cc4f8b
LeeChangHoon <changhoon1030@gmail.com>
2022-11-02 04:17:25 +0900

    ⭐ feat : fuelType,segment 파라미터로 받아서carList로 전달

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,16 @@
-const HomePage = () => {
+import { useRecoilValue } from "recoil"
+import FilterList from "../components/cars/FilterList"
+import { carList } from "../recoil/carList"
+import { fuelTypeParameter, segmentParameter } from "../recoil/filterOption"
 
+const HomePage = () => {
+  const fuelType = useRecoilValue(fuelTypeParameter)
+  const segment = useRecoilValue(segmentParameter)
+  const Cars = useRecoilValue(carList({fuelType, segment}))
+  console.log(fuelType, segment, Cars)
   return (
     <div>
-    HomePage
+      <FilterList />
     </div>
   )
 }
---
+++ src/recoil/filterOption.js
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const fuelTypeParameter = atom({
+  key : 'fuelTypeParameter',
+  default : null
+})
+
+export const segmentParameter  = atom({
+  key : 'segmentParameter',
+  default:null
+})
