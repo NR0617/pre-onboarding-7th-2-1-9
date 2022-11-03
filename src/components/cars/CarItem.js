8a2c3fe1d19478d78e3e7731139a53f9e5e800ae
LeeChangHoon <changhoon1030@gmail.com>
2022-11-03 16:39:48 +0900

    ⚒ refactor : 코드 리팩터링

--- src/components/cars/CarItem.js
+++ src/components/cars/CarItem.js
@@ -1,9 +1,10 @@
-import { useNavigate } from "react-router-dom"
 import tw from 'tailwind-styled-components'
+import { useNavigate } from "react-router-dom"
 import { CarImg, BoldText } from "../../styles/CommonStyle"
-import { convertComma } from "../../utils/convertComma"
+import { convertComma } from "../../utils/convertFn"
 import { isNew } from "../../utils/isNew"
 import NewMark from './NewMark'
+import { convertSegmentToKr, convertFuelTypeToKr } from "../../utils/convertFn"
 const CarItem = ({ car }) => {
   const navigate = useNavigate()
   const handleClickCarInfo = () => {
@@ -15,12 +16,12 @@ const CarItem = ({ car }) => {
       <CarInfo>
         <BoldText>{car.attribute.brand}</BoldText>
         <BoldText>{car.attribute.name}</BoldText>
-        <div>{car.attribute.segment} / {car.attribute.fuelType}</div>
+        <div>{convertSegmentToKr(car.attribute.segment)} / {convertFuelTypeToKr(car.attribute.fuelType)}</div>
         <div>월 {commaAmount}원 부터</div>
       </CarInfo>
       <CarImgWrapper>
         <div>
-        {isNew(car.createdAt) && <NewMark />}
+          {isNew(car.createdAt) && <NewMark />}
         </div>
         <CarImg src={car.attribute.imageUrl} alt={car.attribute.name} />
       </CarImgWrapper>
--- src/utils/setMetaTags.js
+++ src/utils/setMetaTags.js
@@ -1,10 +1,11 @@
 export const setMetaTags = (brand, name, amount,imageUrl) => {
+  // console.log(brand, name, amount, imageUrl)
   document
   .querySelector(`meta[property='og:title']`)
   .setAttribute('content', `${brand} ${name}`)
   document
   .querySelector(`meta[property='og:description']`)
-  .setAttribute('content', `dnjf ${amount} 원`)
+  .setAttribute('content', `월 ${amount} 원`)
   document
   .querySelector(`meta[property='og:image']`)
   .setAttribute('content', `${imageUrl}`)
