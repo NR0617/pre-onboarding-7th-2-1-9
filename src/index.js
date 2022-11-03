48fc9df789cb6802188097420d2c265d2b38245c
LeeChangHoon <changhoon1030@gmail.com>
2022-11-03 20:26:14 +0900

    🧱 build : install react-snap SEO

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { hydrate } from 'react-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+
+if(root.hasChildNodes()){
+hydrate(
+    <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+  )
+} else{
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+
